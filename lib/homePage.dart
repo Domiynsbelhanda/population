8182134986476b7f36fca2badb2767fb050cf61c
Youness Dominique <youness.dominique@gmail.com>
2024-07-29 16:14:25 +0200

    feat: finish population app

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,12 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_countdown_timer/current_remaining_time.dart';
+import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:population/services.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +16,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<HomeScreen> {
+
+  bool _isLoading = false;
+  int endTime = DateTime.now().millisecondsSinceEpoch + 1000 * 60 * 10; // 10 minutes
 
 
   @override
@@ -27,7 +38,7 @@ class _MyHomePageState extends State<HomeScreen> {
                   shape: const CircleBorder(), // Rendre le bouton rond
                   padding: const EdgeInsets.all(24) // Espace interne pour agrandir le bouton
               ),
-              child: const Column(
+              child: _isLoading ? const CircularProgressIndicator() : const Column(
                 mainAxisSize: MainAxisSize.min, // Pour que la colonne ne prenne pas toute la largeur disponible
                 children: <Widget>[
                   Icon(
@@ -81,8 +92,7 @@ class _MyHomePageState extends State<HomeScreen> {
                   )
               ),
               onPressed: () {
-                // Ici, vous pouvez ajouter la logique pour traiter le numéro de téléphone
-                print('Le numéro de téléphone est : ${phoneController.text}');
+                _sendAlert(phoneController.text);
                 Navigator.of(context).pop(); // Ferme la boîte de dialogue
               },
             ),
@@ -91,4 +101,128 @@ class _MyHomePageState extends State<HomeScreen> {
       },
     );
   }
+
+  void _sendAlert(String phoneNumber) async {
+    setState(() {
+      _isLoading = true;  // Activer l'indicateur de chargement
+    });
+
+    try {
+      Position position = await determinePosition();
+
+      // Construire l'URL de l'API
+      var url = Uri.parse('https://tableau.ourworldtkpl.com/api/alertes');
+
+      // Préparer le corps de la requête
+      var body = jsonEncode({
+        'pseudo': UserPreferences.getPseudo(),  // Assurez-vous de récupérer le pseudo stocké localement
+        'telephone': phoneNumber,
+        'latitude': position.latitude.toString(),
+        'longitude': position.longitude.toString(),
+      });
+
+      // Envoyer la requête POST
+      var response = await http.post(url, headers: {
+        'Content-Type': 'application/json'
+      }, body: body);
+
+      if (response.statusCode == 201) {
+        _showSuccessDialog(context);
+      } else {
+        _showErrorDialog(context, phoneNumber, 'Erreur ${response.statusCode}');
+      }
+    }
+    catch (e) {
+      _showErrorDialog(context, phoneNumber, 'Erreur $e');
+    }
+    finally {
+      setState(() {
+        _isLoading = false;  // Désactiver l'indicateur de chargement après la requête
+      });
+    }
+  }
+
+  void _showSuccessDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        int remainingMinutes = 10;  // Initialiser avec 10 minutes
+
+        return AlertDialog(
+          title: const Text("Alerte envoyée avec succès!"),
+          content: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              Timer.periodic(const Duration(minutes: 1), (Timer timer) {
+                if (remainingMinutes == 0) {
+                  timer.cancel();
+                  Navigator.of(context).pop();
+                } else {
+                  setState(() {
+                    remainingMinutes--;
+                  });
+                }
+              });
+
+              return Text("Si vous ne recevez pas un appel à la fin du compteur de $remainingMinutes minutes, rélancez l'alerte");
+            },
+          ),
+          actions: <Widget>[
+            Center(
+            child: CountdownTimer(
+              endTime: endTime,
+              widgetBuilder: (_, CurrentRemainingTime? time) {
+                if (time == null) {
+                  return const Text("Le compteur est terminé",
+                      style: TextStyle(fontSize: 20, color: Colors.red, fontWeight: FontWeight.bold));
+                }
+                return Text(
+                  'Temps restant: ${time.min ?? 0} minutes ${time.sec ?? 0} secondes',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                );
+              },
+              onEnd: () {
+                Navigator.of(context).pop();
+              },
+            )),
+            TextButton(
+              child: const Text("Fermer"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+  void _showErrorDialog(BuildContext context, String phoneNumber, String e) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Une erreur s'est produite"),
+          content: Text('$e'),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Annuler"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: Text("Recommencer"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _sendAlert(phoneNumber); // Réessayer avec les mêmes informations
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
 }
--- lib/services.dart
+++ lib/services.dart
@@ -1,3 +1,4 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserPreferences {
@@ -14,4 +15,39 @@ class UserPreferences {
   static String? getPseudo() {
     return _preferences?.getString('pseudo');
   }
+}
+
+
+Future<Position> determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Vérifie si le service de localisation est activé.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Les services de localisation ne sont pas activés, ne continuez pas
+    // à accéder à la position et demandez à l'utilisateur de les activer.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Les permissions sont refusées, la prochaine fois vous pourriez demander
+      // les permissions encore (le meilleur endroit pour cela est le
+      // code lié à l'action de l'utilisateur qui nécessite la localisation).
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Les permissions sont refusées pour toujours, les requêtes de permissions
+    // ne peuvent plus aider.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // Lorsque les permissions sont accordées, continuez à accéder à la position de l'appareil.
+  return await Geolocator.getCurrentPosition();
 }
